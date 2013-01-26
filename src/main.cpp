2a996edd1f5762025c2538d820de0b3848069ae3
Chris Patuzzo <chris@patuzzo.co.uk>
2013-01-26 00:17:34 +0000

    Pass an image as a parameter

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,17 @@ IplImage *solve(IplImage *magicEye) {
 }
 
 int main(int argc, char **argv) {
-  IplImage *magicEye = cvLoadImage("foo.jpg");
+  if (argc < 2){
+    printf("Usage: magic_eye <file-name>\n\n");
+    exit(0);
+  }
+
+  IplImage *magicEye = cvLoadImage(argv[1]);
+  if (!magicEye){
+    printf("Could not load magic eye: %s\n", argv[1]);
+    exit(0);
+  }
+
   IplImage *solution = solve(magicEye);
 
   cvNamedWindow("Magic Eye", CV_WINDOW_AUTOSIZE);
